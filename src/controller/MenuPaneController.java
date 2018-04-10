6b110ed01c5301dae9e5a873c6fb9e501eca3efd
Stanislaw Rusnak <stanislaw.rusnak1@gmail.com>
2018-04-10 22:25:05 +0200

    Dodanie tabeli na dane, kontrolerów, klasy stylów css, logo, okienka 'informacje o programie'

--- src/controller/MenuPaneController.java
+++ src/controller/MenuPaneController.java
@@ -1,12 +1,20 @@
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.Initializable;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
 public class MenuPaneController implements Initializable {
     @FXML
     private Menu productMenu;
@@ -65,6 +73,23 @@ public class MenuPaneController implements Initializable {
 	
 	
 	private void configureMenu() {
-		
-	}
+        closeMenuItem.setOnAction(x -> Platform.exit());
+ 
+        aboutMenuItem.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent arg0) {
+                Parent parent = null;
+                try {
+                    parent = FXMLLoader.load(getClass().getResource("/view/AboutPane.fxml"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                Scene scene = new Scene(parent);
+                Stage stage = new Stage();
+                stage.setTitle("Manufacture CAT v0.1 - informacje");
+                stage.setScene(scene);
+                stage.show();
+            }
+        });
+    }
 }
